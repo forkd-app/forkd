126b9a4c81eba51b0ced54a0d7ecba0db3d833e9
Your Name~ <tcummings615@gmail.com>
2025-01-06 23:24:24 -0500

    Updated migrations to match new data model. Taskfile: added drop table

--- db/migrations/1726615439_init.down.sql
+++ db/migrations/1726615439_init.down.sql
@@ -1,9 +1,13 @@
 -- Write your down sql migration here
+DROP TABLE IF EXISTS magic_links;
+DROP TABLE IF EXISTS sessions;
+DROP TABLE IF EXISTS ratings;
 DROP TABLE IF EXISTS recipe_ingredients;
-DROP TABLE IF EXISTS recipe_comments ;
 DROP TABLE IF EXISTS ingredient_tags;
 DROP TABLE IF EXISTS measurement_units_tags;
 DROP TABLE IF EXISTS recipe_steps;
+ALTER TABLE recipes
+DROP COLUMN IF EXISTS featured_revision;
 DROP TABLE IF EXISTS recipe_revisions;
 DROP TABLE IF EXISTS measurement_units;
 DROP TABLE IF EXISTS ingredients;
@@ -11,3 +15,4 @@ DROP TABLE IF EXISTS tags;
 DROP TABLE IF EXISTS linked_recipes;
 DROP TABLE IF EXISTS recipes;
 DROP TABLE IF EXISTS users;
+
--- db/migrations/1726615439_init.up.sql
+++ db/migrations/1726615439_init.up.sql
@@ -1,40 +1,48 @@
 -- Write your up sql migration here
 CREATE TABLE IF NOT EXISTS users (
   id bigserial PRIMARY KEY,
-  username varchar(50) NOT NULL UNIQUE,
+  display_name varchar(50) NOT NULL UNIQUE,
   email varchar(255) NOT NULL UNIQUE,
   join_date timestamp NOT NULL DEFAULT now(),
   updated_at timestamp NULL 
 );
+
 CREATE TABLE IF NOT EXISTS recipes (
   id bigserial PRIMARY KEY,
-  featured_revision bigint NOT NULL CONSTRAINT fk_recipe_revision_id REFERENCES recipe_revisions(id),
   author_id bigint NOT NULL CONSTRAINT fk_recipe_author REFERENCES users(id),
   forked_from bigint CONSTRAINT fk_recipe_fork REFERENCES recipes(id),
   slug varchar(75) NOT NULL UNIQUE,
   private boolean NOT NULL,
-  description text,
   initial_publish_date timestamp NOT NULL DEFAULT now()
 );
 
 CREATE TABLE IF NOT EXISTS recipe_revisions (
   id bigserial PRIMARY KEY,
   recipe_id bigint NOT NULL CONSTRAINT fk_recipe_revisions REFERENCES recipes(id),
-  parent_id bigint NOT NULL CONSTRAINT fk_recipe_revision_parent REFERENCES recipe_revisions(id),
-  child_id bigint CONSTRAINT fk_recipe_revision_child REFERENCES recipe_revisions(id),
-  -- Free form content, maybe like an "about" section. Maybe this should be like explaining the changes made
-  description text,
+  parent_id bigint NULL CONSTRAINT fk_recipe_revision_parent REFERENCES recipe_revisions(id),
+  -- About section for the recipe.
+  recipe_description text,
+  --change description should be a note describing how description changed
+  change_comment text,
   title text,
   publish_date timestamp NOT NULL DEFAULT now()
 );
+
+ALTER TABLE IF EXISTS recipes
+ADD featured_revision bigint NULL CONSTRAINT fk_recipe_revision_id REFERENCES recipe_revisions(id);
+
+   
 CREATE TABLE IF NOT EXISTS linked_recipes (
   from_recipe_id bigint NOT NULL CONSTRAINT fk_linked_recipes_from REFERENCES recipes(id),
   to_recipe_id bigint NOT NULL CONSTRAINT fk_linked_recipes_to REFERENCES recipes(id),
   CONSTRAINT linked_recipe_pk PRIMARY KEY(from_recipe_id, to_recipe_id)
 );
 CREATE TABLE IF NOT EXISTS tags (
-  name varchar(255) PRIMARY KEY,
-  description text
+  id bigserial PRIMARY KEY,
+  name varchar(255) NOT NULL UNIQUE,
+  description text,
+  --True if by user , False if internal keyword
+  user_generated boolean NOT NULL
 );
 CREATE TABLE IF NOT EXISTS measurement_units (
   name varchar(255) PRIMARY KEY,
@@ -60,9 +68,28 @@ CREATE TABLE IF NOT EXISTS recipe_ingredients (
   unit varchar(255) NOT NULL CONSTRAINT fk_recipe_ingredient_quantity REFERENCES measurement_units(name),
   comment text
 );
+
 CREATE TABLE IF NOT EXISTS recipe_steps (
   id bigserial PRIMARY KEY,
   revision_id bigint NOT NULL CONSTRAINT fk_recipe_revision_ingredients REFERENCES recipe_revisions(id),
   content text NOT NULL,
   index int NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ratings(
+  id bigserial PRIMARY KEY,
+  revision_id bigint NOT NULL CONSTRAINT fk_recipe_revision_ingredients REFERENCES recipe_revisions(id),
+  --should be 1-5
+  star_value smallint CONSTRAINT check_star_Number CHECK(star_value >= 1 AND star_value  <= 5)
+);
+CREATE TABLE IF NOT EXISTS magic_links(
+  id bigserial PRIMARY KEY,
+  author_id bigint NOT NULL CONSTRAINT fk_magic_link REFERENCES users(id),
+  token varchar (255) NOT NULL,
+  expiry timestamp NOT NULL CHECK (expiry > now())
+);
+CREATE TABLE IF NOT EXISTS sessions(
+  id bigserial PRIMARY KEY,
+  user_id bigint NOT NULL CONSTRAINT fk_session_user REFERENCES users(id),
+  expiry  timestamp NOT NULL CHECK (expiry > now())
 );
