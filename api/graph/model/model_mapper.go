f65a9886d9ef133791a484d4714fe3a5dbd2e556
Graham Vasquez <graham@gvasquez.dev>
2025-01-29 21:23:20 -0500

    refactor: removed unused functions, renamed file to be more clear

--- api/graph/model/model_mapper.go
+++ api/graph/model/model_mapper.go
@@ -3,29 +3,8 @@ package model
 import (
 	"forkd/db"
 
-	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgtype"
 )
-
-func MapStringToPgUuid(str string) (pgtype.UUID, error) {
-	id, err := uuid.Parse(str)
-	if err != nil {
-		return pgtype.UUID{}, err
-	}
-	return pgtype.UUID{
-		Bytes: id,
-		Valid: true,
-	}, nil
-}
-
-func MapPgUuidToString(id pgtype.UUID) (string, error) {
-	goUuid, err := uuid.FromBytes(id.Bytes[:])
-	if err != nil {
-		return "", err
-	}
-
-	return goUuid.String(), nil
-}
 
 func RecipeFromDBType(result db.Recipe) *Recipe {
 	recipe := Recipe{
