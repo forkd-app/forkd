e3b1c7916aadedc68c41a77dfd704f2c31a3d6af
christina.brgs@gmail.com <christina.brgs@gmail.com>
2025-03-14 17:32:13 -0400

    created log in and sign up page

--- web/app/routes/_auth.login/route.tsx
+++ web/app/routes/_auth.login/route.tsx
@@ -1,5 +1,38 @@
-import { Text } from "@mantine/core"
+import { Container, Text, Input, Button } from "@mantine/core"
+import { IconMail } from "@tabler/icons-react"
+import { Form } from "@remix-run/react"
+import { useState } from "react"
 
 export default function LogIn() {
-  return <Text> Log In Page </Text>
+  const [email, setEmail] = useState<string>("")
+
+  const handleLogIn = () => {}
+
+  return (
+    <Container
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+      fluid
+    >
+      <Form style={{ width: 300 }} onSubmit={handleLogIn}>
+        <Text style={{ marginBottom: 10 }}>LOGIN</Text>
+        <Input
+          type="email"
+          placeholder="Your email"
+          leftSection={<IconMail size={16} />}
+          value={email}
+          onChange={(event) => setEmail(event.currentTarget.value)}
+        />
+
+        <Button style={{ width: "100%", marginTop: 15 }} type="submit">
+          login
+        </Button>
+      </Form>
+    </Container>
+  )
 }
