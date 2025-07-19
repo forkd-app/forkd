780527b06c8071ec5c90ea2b6907ceceb96e7452
Graham Vasquez <graham@gvasquez.dev>
2025-07-18 22:13:13 -0400

    refactor: run sqlfluff on migrations

--- db/migrations/20250705235521_recipe_revision_search_index.up.sql
+++ db/migrations/20250705235521_recipe_revision_search_index.up.sql
@@ -1,3 +1,10 @@
 CREATE INDEX recipe_revisions_search_text
 ON recipe_revisions
-USING GIN ((setweight(to_tsvector('english', title), 'A') || setweight(to_tsvector('english', coalesce(recipe_description, '')), 'B')))
+USING gin (
+    (
+        setweight(to_tsvector('english', title), 'A')
+        || setweight(
+            to_tsvector('english', coalesce(recipe_description, '')), 'B'
+        )
+    )
+)
